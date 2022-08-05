4741327a8a7f05ec283daea8cb1d9f4e127bbbd8
Tyler Souza <tyler6souza@hotmail.com>
2022-08-05 16:09:41 -0400

    Init

---
+++ Censor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AeroltChatServer
+{
+
+
+    public class Censor
+    {
+        /// <summary>
+        /// Get or set the censored words list.
+        /// </summary>
+        /// <value>The censored words list.</value>
+        public IList<string> CensoredWords { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Censor"/> class.
+        /// </summary>
+        /// <param name="censoredWords">Censored words, if null uses default list</param>
+        public Censor(IEnumerable<string> censoredWords = null) =>
+            CensoredWords = censoredWords == null ? new List<string>() : new List<string>(censoredWords);
+
+        /// <summary>
+        /// Censors the text and replaces dirty words with ****
+        /// </summary>
+        /// <returns>The text that is now censored</returns>
+        /// <param name="text">Text to censor</param>
+        public string CensorText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return text;
+
+            var censoredText = text;
+
+            foreach (string censoredWord in CensoredWords)
+            {
+                var regularExpression = ToRegexPattern(censoredWord);
+
+                censoredText = Regex.Replace(censoredText, regularExpression, StarCensoredMatch,
+                    RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            }
+
+            return censoredText;
+        }
+
+        /// <summary>
+        /// Determines whether the text is dirty (has a bad word in it).
+        /// </summary>
+        /// <returns><c>true</c> if text is dirty; otherwise, <c>false</c>.</returns>
+        /// <param name="text">Text to check for dirty words.</param>
+        public bool HasCensoredWord(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
+            var censoredText = text;
+
+            foreach (var censoredWord in CensoredWords)
+            {
+                var regularExpression = ToRegexPattern(censoredWord);
+
+                censoredText = Regex.Replace(censoredText, regularExpression, StarCensoredMatch,
+                    RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+                if (censoredText != text)
+                    return true;
+            }
+
+            return false;
+        }
+
+        static string StarCensoredMatch(Group m) =>
+            new string('*', m.Captures[0].Value.Length);
+
+        static string ToRegexPattern(string wildcardSearch)
+        {
+            var regexPattern = Regex.Escape(wildcardSearch);
+
+            regexPattern = regexPattern.Replace(@"\*", ".*?");
+            regexPattern = regexPattern.Replace(@"\?", ".");
+
+            if (regexPattern.StartsWith(".*?", StringComparison.Ordinal))
+            {
+                regexPattern = regexPattern.Substring(3);
+                regexPattern = @"(^\b)*?" + regexPattern;
+            }
+
+            regexPattern = @"\b" + regexPattern + @"\b";
+
+            return regexPattern;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace AeroltChatServer
+{
+    
+// State object for reading client data asynchronously  
+    class Server
+    {
+        // add array to store current users
+        private partial class UserList
+        {
+            public static List<string> Usernames = new List<string>();
+        }
+
+        private class Connect : WebSocketBehavior
+        {
+            private string _name;
+
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + e.Data + " connected");
+                _name = e.Data;
+                Sessions.Broadcast("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + "Welcome " + e.Data +" to the server!");
+            }
+
+            protected override void OnClose(CloseEventArgs e)
+            {
+                //Sessions.Broadcast("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + _name + " has left.");
+            }
+        }
+
+        private class Usernames : WebSocketBehavior
+        {
+            
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                var usernameCensored = FilterText(e.Data);
+                UserList.Usernames.Add(usernameCensored);
+                Sessions.Broadcast(string.Join("\n", UserList.Usernames));
+            }
+
+            protected override void OnClose(CloseEventArgs e)
+            {
+                Sessions.Broadcast(string.Join("\n", UserList.Usernames));
+            }
+        }
+
+        private class Disconnect : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + e.Data + " disconnected");
+                //Sessions.Broadcast("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + e.Data + " has left.");
+
+                UserList.Usernames.Remove(e.Data);
+            }
+        }
+        private class UserCount : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Sessions.Broadcast($"{UserList.Usernames.Count}");
+            }
+            protected override void OnClose(CloseEventArgs e)
+            {
+                Sessions.Broadcast($"{UserList.Usernames.Count}");
+            }
+        }
+
+        private class SendMessage : WebSocketBehavior
+        {
+            
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] " + e.Data);
+                if (e.Data != null)
+                {
+                    if (e.Data.Contains("#"))
+                    {
+                        string[] SplitMsg = e.Data.Split('#');
+                       
+                        string joinLobbyLink =  string.Format("<#7f7fe5><u><link=\"{0}\">{1}</link></u></color>", SplitMsg[1], $"{SplitMsg[0]} Join My Lobby!");
+                        Sessions.Broadcast(joinLobbyLink);
+                        return;
+                    }
+                    var censoredMsg = FilterText(e.Data);
+                    string msg= $"<noparse>{censoredMsg}</noparse>";
+                    Sessions.Broadcast(msg);
+                    
+                }
+            }
+        }
+
+        private static string FilterText(string textToFilter)
+        {
+            var censor = new Censor(ProfanityBase._wordList);
+            var censored = censor.CensorText(textToFilter);
+            return censored;
+        }
+        
+        private static void RunServer()
+        {
+            var ip = IPAddress.Any;
+            var port = 5000;
+            
+            var server = new WebSocketServer($"ws://{ip}:{port}");
+            server.AddWebSocketService<SendMessage>("/Message");
+            server.AddWebSocketService<Usernames>("/Usernames");
+            server.AddWebSocketService<Disconnect>("/Disconnect");
+            server.AddWebSocketService<UserCount>("/UserCount");
+            server.AddWebSocketService<Connect>("/Connect");
+            
+            server.Start();
+            Console.WriteLine($"Server started on {ip} listening on port {port}...");
+            Console.WriteLine("Waiting for connections...");
+            
+            Console.ReadKey(true);
+        }
+
+        public static void Main(string[] args)
+        {
+            RunServer();
+        }
+    }
+}
